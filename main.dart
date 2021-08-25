cbb9d1cb4ba10ffdfefb088564012d9a991dfa6b
Eder <haydukieder@gmail.com>
2021-08-24 21:46:04 -0300

    Criando columns

--- main.dart
+++ main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.00'),
+                subtitle: Text('2000')
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.00'),
+                subtitle: Text('1000')
+              )
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferência'),
         ),
