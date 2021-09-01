946fe592f819b500651f01a6ba606bc6187f2813
Eder <haydukieder@gmail.com>
2021-09-01 19:52:14 -0300

    Colocando as labels nos campos de texto.

--- main.dart
+++ main.dart
@@ -26,32 +26,17 @@ class FormularioTransferencia extends StatelessWidget {
         title: Text('Criando Transferência'),
       ),
       body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controllerNumeroConta,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  labelText: 'Número da Conta', hintText: '0000'),
-              keyboardType: TextInputType.number,
-            ),
+        children: <Widget>[
+          Editor(
+            _controllerNumeroConta,
+            'Número da Conta',
+            '0000',
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controllerValor,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0.00'),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+            _controllerValor,
+            'Valor',
+            '0.00',
+            icone: Icons.monetization_on,
           ),
           ElevatedButton(
             onPressed: () {
@@ -61,6 +46,7 @@ class FormularioTransferencia extends StatelessWidget {
               final valor = double.tryParse(_controllerValor.text);
               debugPrint('$valor');
               final numeroConta = int.tryParse(_controllerNumeroConta.text);
+
 
               if (numeroConta != null && valor != null) {
                 //Transferencia(valor, numeroConta);
@@ -142,28 +128,33 @@ class ItemTransferencia extends StatelessWidget {
 }
 
 class Editor extends StatelessWidget {
-  final TextEditingController _controller;
-  final String _rotulo;
-  final String _dica;
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone;
 
-  Editor(this._controller, this._rotulo, this._dica);
+  Editor(
+      this.controlador,
+      this.rotulo,
+      this.dica,
+      {this.icone}); // ?->Permite acesso nullo
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: TextField(
-        controller: _controller,
+        controller: controlador,
         style: TextStyle(
           fontSize: 24.0,
         ),
         decoration: InputDecoration(
-            icon: Icon(Icons.monetization_on),
-            labelText: 'Valor',
-            hintText: '0.00'),
+          icon: icone != null ? Icon(icone) : null ,
+          labelText: rotulo,
+          hintText: dica,
+        ),
         keyboardType: TextInputType.number,
       ),
-    ) ;
+    );
   }
 }
-
